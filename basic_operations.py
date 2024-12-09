afc98d9092f478cb741c32fce983f0a2f6bd3c85
Nehemiah <nehemynet@gmail.com>
2024-12-09 23:16:10 +0100

    file commit

---
+++ basic_operations.py
@@ -0,0 +1,21 @@
+number1 = 10 
+number2 = 5
+
+
+Addition = number1 + number2
+
+difference = number2 - number1
+
+product = number1 * number2
+
+
+print(f"Addition of {number1} and {number2} is {Addition}")
+print(f"Substraction of {number2} and {number1} is {difference}")
+print(f"Multiplication  of {number1} and {number2} is {product}")
+
+
+
+
+
+
+
