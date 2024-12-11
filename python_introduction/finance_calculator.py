5490d9d4a914119153b3857061e80ade633a85e9
Nehemy <nehemynet@gmail.com>
2024-12-11 12:58:17 +0100

    File added

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,7 +8,5 @@ m_savings = m_income - m_expenses
 
 p_savings = m_savings * 12 + (m_savings * 12 * 0.05)
 
-print(f"Enter your monthly income: ${m_income}")
-print(f"Enter your total monthly expenses: ${m_expenses}")
 print(f"Your monthly savings are: ${m_savings}")
 print(f"Projected savings after one year, with interest, is: ${p_savings}")
