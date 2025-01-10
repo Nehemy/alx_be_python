fdee741dd19f58b8bcdcb12e081cdde6be761627
Nehemy <nehemynet@gmail.com>
2025-01-10 04:05:36 +0100

    Commit

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num1 / 0:
+            print("Number cannot be divided by zero")
+        else:
+            return num1 / num2
