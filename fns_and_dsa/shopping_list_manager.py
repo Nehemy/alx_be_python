f5568d7cebef503e84892e0f06cdaf6d6a25e8be
Nehemy <nehemynet@gmail.com>
2025-01-10 04:25:00 +0100

    Commit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,33 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_item = input("Enter the name of the item: ")
+            shopping_list.append(add_item)
+            
+        elif choice == '2':
+            remove_item = input("Enter the name of the item to remove: ")
+            if remove_item not in shopping_list:
+                print("Item not in shopping list")
+            else:
+                shopping_list.remove(remove_item)
+            
+        elif choice == '3':
+            for i in shopping_list:
+                print(i)
+            
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
