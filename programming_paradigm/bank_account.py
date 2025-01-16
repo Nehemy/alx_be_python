a5d6b709a5ff7bbb109a1001a25d753e35031f98
Nehemy <nehemynet@gmail.com>
2025-01-16 17:39:41 +0100

    Commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount():
+    
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        
+        
+    def deposit(self, amount):
+        self.amount = amount
+        self.account_balance += self.amount
+        
+        
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.amount <= self.account_balance:
+            self.account_balance -= self.amount
+            return True
+        else:
+            return False
+    
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
