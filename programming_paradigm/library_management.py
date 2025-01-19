145ff49eed8583fa82db2c51be17048696a4dc84
Nehemy <nehemynet@gmail.com>
2025-01-19 17:28:17 +0100

    commit

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -8,15 +8,28 @@ class Book:
 class Library:
     def __init__(self):
         self._books = []
-    
-    def add_book(self, Book):
-        pass
-    
-    def return_book(self, book):
-        pass
         
-    def check_out_book(title):
-        pass
     
+    def add_book(self, book):
+        self._books.append(book)
+        for book in self._books:
+            if book._is_checked_out == False:
+                print(f"Available books after setup:\n{book._title} by {book._author}")
+    
+    def check_out_book(self, title):
+        for book in self._books:
+            if book._title == title:
+                book._is_checked_out = True
+                print(f"Available books after checking out '{book._title}':\n{book._title} by {book._author}")
+    
+    def return_book(self, title):
+        for book in self._books:
+            if book._title == title:
+                book._is_checked_out = False
+                print(f"Available books after returning '{book._title}':\n{book._title} by {book._author}")
+
+
     def list_available_books(self):
-        return self._books
+        for book in self._books:
+            if book._is_checked_out == False:
+                print(f"Available books after checking out {book._title}:\n{book._title} by {book._author}")
